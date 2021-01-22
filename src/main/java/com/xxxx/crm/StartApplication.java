3ead818be2a0ecc7d5ac68886c768f4bd9341471
baixiaopang <2986085560@qq.com>
2021-01-22 20:57:16 +0800

    项目搭建：登录页和主页

---
+++ src/main/java/com/xxxx/crm/StartApplication.java
@@ -0,0 +1,16 @@
+package com.xxxx.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author arthur
+ * @date 2021/1/22 17:37
+ */
+@SpringBootApplication
+public class StartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class);
+    }
+}
