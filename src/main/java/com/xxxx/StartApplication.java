c480ff4773d771643a96f6b07279695afc068faa
baixiaopang <2986085560@qq.com>
2021-01-23 15:05:40 +0800

    login登录和逆向工程

--- src/main/java/com/xxxx/StartApplication.java
+++ src/main/java/com/xxxx/StartApplication.java
@@ -1,5 +1,6 @@
-package com.xxxx.crm;
+package com.xxxx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/1/22 17:37
  */
 @SpringBootApplication
+@MapperScan("com.xxxx.crm.dao")
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class);
