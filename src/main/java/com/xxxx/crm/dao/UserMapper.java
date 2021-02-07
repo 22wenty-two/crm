912d1890e5bd75ca51611e78ddd183fb1bb46e92
baixiaopang <2986085560@qq.com>
2021-02-07 16:25:37 +0800

    项目搭建：主页完成

--- src/main/java/com/xxxx/crm/dao/UserMapper.java
+++ src/main/java/com/xxxx/crm/dao/UserMapper.java
@@ -3,6 +3,9 @@ package com.xxxx.crm.dao;
 import com.xxxx.base.BaseMapper;
 import com.xxxx.crm.vo.User;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author NeXT
  */
@@ -13,4 +16,16 @@ public interface UserMapper extends BaseMapper<User,Integer> {
      * @return
      */
     User queryUserByUserName(String userName);
+
+    /**
+     * 查询分配人
+     * @return
+     */
+    List<Map<String,Object>> queryAllSales();
+
+    /**
+     * 服务创建-服务处理：客户经理
+     * @return
+     */
+    List<Map<String, Object>> queryAllCustomerManager();
 }
